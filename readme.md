a1ce3831ede15debd3e84f663b9330386b7d5b58
divine <cdivine304@gmail.com>
2019-12-03 14:20:49 +0530

    updated

---
+++ readme.md
@@ -0,0 +1,6 @@
+https://stackoverflow.com/questions/37072368/retrofit-body-with-multipart-having-issue
+
+
+https://square.github.io/retrofit/#download
+
+
--- src/com/im/IMMain.java
+++ src/com/im/IMMain.java
@@ -2,6 +2,8 @@ package com.im;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -13,16 +15,21 @@ import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.PartMap;
+import retrofit2.http.Path;
 
 
 interface GitHubService {
 	
 	@Multipart
 	@POST("im-j2ee/api/agent")
-	Call<String> uploadAttachment(@Part MultipartBody.Part filePart);
+	Call<String> uploadAttachment(
+			@Part MultipartBody.Part filePart,
+			@PartMap Map<String,RequestBody> params);
 
 }
 
@@ -33,8 +40,8 @@ public class IMMain {
 		        .setLenient()
 		        .create();
 		
-		String filepath = "/Users/dev/Applications/codes/java/eclipse_ws/ws1/im-restclient/pic412.jpg";
-		
+		//String filepath = "/Users/dev/Applications/codes/java/eclipse_ws/ws1/im-restclient/pic512.jpg";
+		String filepath = "/Users/dev/Applications/codes/java/eclipse_ws/ws1/im-restclient/demo123.csv";
 		Retrofit retrofit = new Retrofit.Builder().baseUrl("http://localhost:8080/")
 				.addConverterFactory( GsonConverterFactory.create(gson)).build();
 		
@@ -45,7 +52,12 @@ public class IMMain {
 		MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", file.getName(),
 				RequestBody.create(MediaType.parse("image/*"), file));
 
-		Call<String> call = service.uploadAttachment(filePart);
+		Map<String, RequestBody> map = new HashMap<>();
+		
+		map.put("city", RequestBody.create(MediaType.parse("multipart/form-data"), "delhi"));
+		map.put("country", RequestBody.create(MediaType.parse("multipart/form-data"), "china"));
+
+		Call<String> call = service.uploadAttachment(filePart,map);
 		Response<String> resp = call.execute();
 		System.out.println(resp);
 		System.out.println(resp.body());
